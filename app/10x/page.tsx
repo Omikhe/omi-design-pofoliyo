aaaf16aa2be212e585704cc6c4083dbac8c47f19
Omikhe <msakamike84@gmail.com>
2024-11-13 04:55:39 +0200

    add unstyled content

--- app/10x/page.tsx
+++ app/10x/page.tsx
@@ -1,4 +1,13 @@
 export default function page() {
     return (<main className="px-[16px]">
+        <h1>Omikhe</h1>
+        <h2>Graphic Designer</h2>
+        <ul>
+            <li>WhatsApp</li>
+            <li>Email</li>
+            <li>Instagram</li>
+            <li>Behence</li>
+        </ul>
+        <h3>© 2024 Omikhe </h3>
     </main>);
 }
