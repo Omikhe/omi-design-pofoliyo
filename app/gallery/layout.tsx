263298b0083957b0af39ba39f01a2b1255b2e1df
Omikhe <msakamike84@gmail.com>
2024-11-14 01:23:36 +0200

    add place holder content

--- app/gallery/layout.tsx
+++ app/gallery/layout.tsx
@@ -4,10 +4,10 @@ import '../globals.css';
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <div className="flex h-screen flex-col md:flex-row md:overflow-hidden ">
-            <div className="md:w-[420px] flex-none bg-slate-400">
+            <div className="md:w-[420px] flex-none">
                 <Sidenav />
             </div>
-            <div className="flex-grow md:overflow-y-auto md:pr-[100px] md:pt-[100px]">{children}</div>
+            <div className="flex-grow md:overflow-y-auto md:pr-[50px] md:pt-[50px] md:bg-gray-200">{children}</div>
         </div>
     );
 }
--- app/gallery/logos/page.tsx
+++ app/gallery/logos/page.tsx
@@ -1,26 +1,11 @@
-import Link from 'next/link';
-
 export default function Page() {
     return (
         <main className="h-screen flex flex-col items-center justify-center px-4">
             <div className="text-center space-y-2 mb-8">
-                <h1 className="text-[64px] sm:text-5xl md:text-[64px] font-bold">Omikhe</h1>
-                <h2 className="text-[16px] sm:text-base text-gray-500">Graphic Designer</h2>
+                <h1 className="text-[32px] sm:text-5xl md:text-[64px] font-bold animate-pulse">Site{"'"}s Cooking</h1>
+                <h2 className="text-sm sm:text-base text-gray-500">Thank you for your patience</h2>
             </div>
 
-            <div className="flex flex-wrap justify-center items-center gap-3 sm:gap-4">
-                <Link href="https://wa.me/265985626249">WhatsApp</Link>
-                <div className="w-1 h-1 bg-gray-400 rounded-full"></div>
-                <Link href="mailto:omikhe265@gmail.com">Email</Link>
-                <div className="w-1 h-1 bg-gray-400 rounded-full"></div>
-                <Link href="https://instagram.com/omikhe_">Instagram</Link>
-                <div className="w-1 h-1 bg-gray-400 rounded-full"></div>
-                <Link href="https://www.behance.net/mikemsaka">Behance</Link>
-            </div>
-
-            <div className="absolute bottom-4 text-sm text-gray-500">
-                &#169; 2024 Omikhe
-            </div>
         </main>
     );
 }
