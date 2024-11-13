e06677f2cb77fbbb8925258c95704a2b3f6917ea
Omikhe <msakamike84@gmail.com>
2024-11-13 18:23:00 +0200

    the layout thingy is working!

--- app/gallery/layout.tsx
+++ app/gallery/layout.tsx
@@ -3,7 +3,7 @@ import Sidenav from "./sidenav";
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-            <div className="w-full flex-none md:w-64">
+            <div className="px-[100px] pt-[100px] w-full flex-none md:w-64">
                 <Sidenav />
             </div>
             <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
--- app/gallery/sidenav.tsx
+++ app/gallery/sidenav.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 export default function Sidenav() {
     return (
-        <>
+        <main className="flex h-full flex-col px-3 py-4 md:px-2 bg-slate-600">
             <div>
                 <h1>Mike Andrew Msaka</h1>
                 <p>I stand at the precipice of day and night, my camera a loyal companion in my quest to capture the ineffable. With each sunset I frame, I bear witness to the death and rebirth of possibility—a daily ritual that reminds me that life is a constant interplay of order and chaos.</p>
@@ -13,6 +13,6 @@ export default function Sidenav() {
                     <Link href="https://wa.me/265985626249">ABOUT</Link>
                 </div>
             </div>
-        </>
+        </main>
     );
 }
