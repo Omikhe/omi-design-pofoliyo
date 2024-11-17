cc960715a9240247a74e562615e4ba904dcb5363
Omikhe <msakamike84@gmail.com>
2024-11-17 16:09:28 +0200

    fix lightbox launch issues

--- app/gallery/photography/page.tsx
+++ app/gallery/photography/page.tsx
@@ -10,12 +10,20 @@ import { slides } from "./slides";
 export default function Page() {
     const [open, setOpen] = useState<boolean>(false);
 
+    const [imageIndex, setImageIndex] = useState<number>(0);
+
+    const handleImageClick = (index: number) => {
+        setImageIndex(index);
+        setOpen(true);
+    };
+
     return (
         <main className="flex flex-col items-center justify-center px-4">
             <div className="columns-1 sm:columns-2 lg:columns-4 py-10 md:py-20 gap-4">
                 {/* <button onClick={() => setOpen(true)} className="w-auto bg-slate-400">Open</button> */}
 
                 <Lightbox
+                    index={imageIndex}
                     className="backdrop-blur-lg"
                     plugins={[Captions, Counter, Fullscreen]}
                     counter={{ container: { style: { top: "unset", bottom: "0", right: "0", left: "unset" } } }}
@@ -26,7 +34,7 @@ export default function Page() {
 
                 {slides.map((slide, index) => (
                     <div key={index} className="mb-4 break-inside-avoid">
-                        <button onClick={() => setOpen(true)} className="w-auto bg-slate-400">
+                        <button onClick={() => handleImageClick(index)} className="w-auto bg-slate-400">
                             <img src={slide.src} alt={slide.title} className="w-full object-cover" />
                         </button>
                     </div>
