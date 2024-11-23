93a08659f3d1ba8e1a809621f3b81d41655c66ba
Omikhe <msakamike84@gmail.com>
2024-11-23 18:04:21 +0200

    fix image paths

--- app/gallery/logos/slides.tsx
+++ app/gallery/logos/slides.tsx
@@ -1,33 +1,28 @@
 const slides = [
-    {
-        src: "https://i.pinimg.com/1200x/37/44/ae/3744aea594dfe339a486715113dc6f0d.jpg",
-        title: "Valuations Africa",
-        description: "A modern logo for a real estate valuation company",
-        width: 1200,
-        height: 1200
-    },
-    {
-        src: "https://i.pinimg.com/1200x/68/d4/69/68d4699048bd66362c053b11ae0c127e.jpg",
-        title: "Malu The Comedian",
-        description: "Logo for comedian/influncer, Malumbo Ngaiyaye's, personal merch",
-        width: 1200,
-        height: 1200
-    },
-    {
-        src: "https://i.pinimg.com/1200x/1b/cf/26/1bcf26db15d0d4868ef451be5acaacc0.jpg",
-        title: "Malu The Comedian",
-        description: "Logo for comedian/influncer, Malumbo Ngaiyaye's, fan merch",
-        width: 1200,
-        height: 1200
-    },
-    {
-        src: "https://i.pinimg.com/1200x/f3/2c/73/f32c7351628682219ee9fe7073c2bbce.jpg",
-        title: "Malu The Comedian",
-        description: "Main logo for comedian/influncer, Malumbo Ngaiyaye",
-        width: 1200,
-        height: 1200
-    }
+    { src: "/logos/3x.png", title: "", description: "", width: 1080, height: 1080 },
+    { src: "/logos/amalawi-anzanga.png", title: "", description: "", width: 1080, height: 1080 },
+    { src: "/logos/angie-beauty-parlour.png", title: "", description: "", width: 1080, height: 1080 },
+    { src: "/logos/angie-wigs.png", title: "", description: "", width: 1080, height: 1080 },
+    { src: "/logos/ariaz-phala.png", title: "", description: "", width: 1080, height: 1080 },
+    { src: "/logos/blackspider.png", title: "", description: "", width: 1080, height: 1080 },
+    { src: "/logos/emtiquemedia.png", title: "", description: "", width: 1080, height: 1080 },
+    { src: "/logos/firm-and-strong.png", title: "", description: "", width: 1080, height: 1080 },
+    { src: "/logos/iMORRCS.png", title: "", description: "", width: 1080, height: 1080 },
+    { src: "/logos/let-love-lead.png", title: "", description: "", width: 1080, height: 1080 },
+    { src: "/logos/localman-main.png", title: "", description: "", width: 1080, height: 1080 },
+    { src: "/logos/localman-sub-1.png", title: "", description: "", width: 1080, height: 1080 },
+    { src: "/logos/localman-sub-2.png", title: "", description: "", width: 1080, height: 1080 },
+    { src: "/logos/malu-the-comedian.png", title: "", description: "", width: 1080, height: 1080 },
+    { src: "/logos/omikhe-1.png", title: "", description: "", width: 1080, height: 1080 },
+    { src: "/logos/omikhe-2.png", title: "", description: "", width: 1080, height: 1080 },
+    { src: "/logos/streamlycs.png", title: "", description: "", width: 1080, height: 1080 },
+    { src: "/logos/sweet-grill.png", title: "", description: "", width: 1080, height: 1080 },
+    { src: "/logos/tchaman-main.png", title: "", description: "", width: 1080, height: 1080 },
+    { src: "/logos/tchanman-sub.png", title: "", description: "", width: 1080, height: 1080 },
+    { src: "/logos/valuations-africa.png", title: "", description: "", width: 1080, height: 1080 },
+    { src: "/logos/wizbago.png", title: "", description: "", width: 1080, height: 1080 },
+    { src: "/logos/ziyembe-community-radio.png", title: "", description: "", width: 1080, height: 1080 }
+];
 
-]
 
 export default slides;
