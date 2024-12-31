34cd6300e410ba730c6ee8e478742fdcb50e2cf5
Omikhe <msakamike84@gmail.com>
2024-12-31 20:01:58 +0200

    componentize card

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -9,25 +9,25 @@ interface CardProps {
 }
 
 
-export default function Card({ image, title, year, description }) {
+export default function Card({ image, title, year, description }: CardProps) {
     return (
         <main className="w-96 h-96 bg-white overflow-hidden rounded-lg relative ">
             <Image
-                src={"/branding/valuations-africa.png"}
-                alt={"valuations"}
+                src={image}
+                alt={description}
                 layout="fill"
                 objectFit="cover"
                 // Optional: Add priority for above-the-fold images
                 priority
             />
 
-            <div className="bg-black/85 backdrop-blur-lg gap-2 flex flex-col p-5 absolute bottom-0 left-0">
+            <div className="bg-black/85 backdrop-blur-lg gap-2 flex flex-col p-5 absolute bottom-0 left-0 w-full">
                 <div className="flex justify-between">
-                    <h1 className="text-xl font-semibold">Valuation Africa</h1>
-                    <h2>2024</h2>
+                    <h1 className="text-xl font-semibold">{title}</h1>
+                    <h2>{year}</h2>
                 </div>
 
-                <p className="text-xs">A unique brand identity for a real estate valuation company, focusing on a professional logo, modern visuals, and consistent branding elements to convey trust and expertise.</p>
+                <p className="text-xs">{description}</p>
                 <div className="flex gap-2">
                     <Link href={"#"} className="flex items-center h-8 px-2 py-4 bg-[#F2F2F2] text-black rounded-full text-xs">
                         Case Study
