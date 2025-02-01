b48322e7ced92401c70f327798cbce4e818adfbd
Omikhe <msakamike84@gmail.com>
2025-02-01 21:32:23 +0200

    create file

---
+++ app/components/footer.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+    return (
+        <main className="p-4">
+            <hr />
+            <div>
+                Cooked In Malawi
+            </div>
+        </main>
+    );
+}
