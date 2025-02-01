1397e698d3594be812762588d0669fc30f6adf49
Omikhe <msakamike84@gmail.com>
2025-02-01 22:17:59 +0200

    add placeholder

---
+++ app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <main className="h-screen w-screen flex flex-col items-center justify-center gap-2">
+            <span className="animate-pulse text-4xl font-semibold">Still cooking 👨🏿‍🍳</span>
+            <h2>Thank you for your patience</h2>
+        </main>
+    );
+}
