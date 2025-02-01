c535f6e3c00094c70b5ab3f925248ac2440e9ebb
Omikhe <msakamike84@gmail.com>
2025-02-01 20:47:22 +0200

    add data

---
+++ app/design/dataa.tsx
@@ -0,0 +1,46 @@
+export const branding = [
+    {
+        "title": "Valuations Africa",
+        "image": "/branding/valuations-africa.png",
+        "year": "2024",
+        "description": "A unique brand identity for a real estate valuation company, focusing on a professional logo, modern visuals, and consistent branding elements to convey trust and expertise."
+    },
+    {
+        "title": "Ziyemebe Community Radio",
+        "image": "/branding/ziyemebe-radio.png",
+        "year": "2024",
+        "description": "An elegant brand identity for a local Malawian radio station."
+    },
+    {
+        "title": "Valuations Africa",
+        "image": "/branding/valuations-africa.png",
+        "year": "2024",
+        "description": "A unique brand identity for a real estate valuation company, focusing on a professional logo, modern visuals, and consistent branding elements to convey trust and expertise."
+    },
+    {
+        "title": "Ziyemebe Community Radio",
+        "image": "/branding/ziyemebe-radio.png",
+        "year": "2024",
+        "description": "An elegant brand identity for a local Malawian radio station."
+    }
+
+]
+
+export const categories = [
+    {
+        lebel: "All Projects"
+    },
+    {
+        lebel: "Branding"
+    },
+    {
+        lebel: "Flyers"
+    },
+    {
+        lebel: "Logos"
+    },
+    {
+        lebel: "Birthday Artworks"
+    },
+
+]
