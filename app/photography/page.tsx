39cdd23d9d025fc3f36956a14b1e63294e09a6ea
Omikhe <msakamike84@gmail.com>
2025-02-16 17:13:16 +0200

    change grid structure

--- app/photography/page.tsx
+++ app/photography/page.tsx
@@ -3,14 +3,14 @@ import photoSlides from "./slides"
 
 export default function Page() {
     return (
-        <section className="container mx-auto px-4">
+        <section className="container mx-auto px-4 w-full">
             <div className="my-8">
-                <h2 className="text-4xl font-semibold mb-8">📸 Photography</h2>
+                <h2 className="block md:hidden text-4xl font-semibold mb-8">📸 Photography</h2>
             </div>
             <Gallery
                 slides={photoSlides}
                 columns={{ mobile: 1, tablet: 3, desktop: 4 }} // Custom column layout
-                className="gap-6" // Custom gap
+                className="gap-16" // Custom gap
             />
         </section>
     )
