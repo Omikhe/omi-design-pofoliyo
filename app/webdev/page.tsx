3b16335184eab2913a9f71a559ebb679ee5290d8
Omikhe <msakamike84@gmail.com>
2025-02-16 17:09:41 +0200

    add placeholder

--- app/webdev/page.tsx
+++ app/webdev/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
     return (
-        <main className="h-screen w-screen flex flex-col items-center justify-center gap-2">
+        <main className="h-full w-screen flex flex-col items-center justify-center gap-2">
 
             <span className="animate-pulse text-4xl font-semibold">Still cooking 👨🏿‍🍳</span>
             <h2>Thank you for your patience</h2>
